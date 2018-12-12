5eee86a7aabcedba596983dad913bb4c80bb0e33
Jasdeepsidhu2 <43177779+Jasdeepsidhu2@users.noreply.github.com>
2018-12-11 17:43:10 -0700

    updates

---
+++ javscript/resume.js
@@ -0,0 +1,12 @@
+// *********Resumes Sections /click effect********
+$("#button1").click(function() {
+    $('.transform1').toggleClass('transform1-active');
+  });
+  
+$("#button2").click(function() {
+  $('.transform2').toggleClass('transform2-active');
+});
+
+$("#button3").click(function() {
+   $('.transform3').toggleClass('transform3-active');
+});
